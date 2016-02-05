113426ed1fa55f0d00ce93a9bc36efbe2462293a
rafolo89 <rafolo@hotmail.it>
2016-02-05 17:38:40 +0100

    Aggiornamento funzione Ajax

--- Alumni/js/inviavalore.js
+++ Alumni/js/inviavalore.js
@@ -2,45 +2,13 @@ var xmlHttp
 
 function stampa(str,nomefile, nomediv)
 {
-xmlHttp=GetXmlHttpObject()
-if (xmlHttp==null)
-  {
-  alert ("Your browser does not support AJAX!");
-  return;
-  } 
-url=nomefile+"?valore="+str;
-//Aggiunge un numero a uocchio per nasconde
-url=url+"&sid="+Math.random();
-xmlHttp.onreadystatechange=stateChanged;
-xmlHttp.open("POST",url,true);
-xmlHttp.send(null);
-} 
+      var xmlhttp = new XMLHttpRequest();
+         xmlhttp.onreadystatechange = function() {
+             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                 document.getElementById(nomediv).innerHTML = xmlhttp.responseText;
+             }
+         }
+         xmlhttp.open("POST", nomefile+"?valore="+str, true);
+         xmlhttp.send();
+     }
 
-function stateChanged() 
-{ 
-   if (xmlHttp.readyState==4){ 
-       document.getElementById(nomediv).innerHTML=xmlHttp.responseText;
-   }
-}
-
-function GetXmlHttpObject()
-{
-var xmlHttp=null;
-try
-  {
-  xmlHttp=new XMLHttpRequest();
-  }
-catch (e)
-  {
-  // Internet Explorer
-  try
-    {
-    xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
-    }
-  catch (e)
-    {
-    xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-  }
-return xmlHttp;
-}
