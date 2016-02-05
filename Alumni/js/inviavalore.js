9463c44f575b2aea61c909dbed71b01aa7922908
rafolo89 <rafolo@hotmail.it>
2016-02-05 12:03:52 +0100

    Ajax

---
+++ Alumni/js/inviavalore.js
@@ -0,0 +1,46 @@
+var xmlHttp
+
+function stampa(str,nomefile)
+{
+xmlHttp=GetXmlHttpObject()
+if (xmlHttp==null)
+  {
+  alert ("Your browser does not support AJAX!");
+  return;
+  } 
+url=nomefile+"?valore="+str;
+//Aggiunge un numero a uocchio per nasconde
+url=url+"&sid="+Math.random();
+xmlHttp.onreadystatechange=stateChanged;
+xmlHttp.open("POST",url,true);
+xmlHttp.send(null);
+} 
+
+function stateChanged() 
+{ 
+   if (xmlHttp.readyState==4){ 
+       document.getElementById("post_content").innerHTML=xmlHttp.responseText;
+   }
+}
+
+function GetXmlHttpObject()
+{
+var xmlHttp=null;
+try
+  {
+  xmlHttp=new XMLHttpRequest();
+  }
+catch (e)
+  {
+  // Internet Explorer
+  try
+    {
+    xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+    }
+  catch (e)
+    {
+    xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  }
+return xmlHttp;
+}
