d1b67dc82cb95f0a280137b5de7325dca940c33a
Ale1994 <alessandraorsi.94@libero.it>
2016-02-06 19:02:16 +0100

    Alumno
    Rimodificate le virgolette

--- Alumni/Application/Alumno.php
+++ Alumni/Application/Alumno.php
@@ -5,18 +5,18 @@
  $path="../Application/".basename($_SERVER['PHP_SELF'])
  setcookie("file",, time()+3600);
  if($_POST['valore']==1){
-              $immagine = "../Alumno/uploads/".$_COOKIE['id_utente'].".jpg";
+              $immagine = "../Alumno/uploads/".$_COOKIE['id_utente']." ".jpg";
               echo "<a href='JavaScript:openWindow()' ><img src=$immagine alt='BO'  height='100' width='100' style = 'float : left' onerror=this.src='../images/userpic.gif'></img></a>";
               }
               
  if($_POST['valore']==2){
- 			 setcookie("valore","NOME= , ID= ".$_COOKIE['id_utente'].", time()+3600);
+ 			 setcookie("valore","NOME= , ID= ".$_COOKIE['id_utente']." ", time()+3600);
   			 setcookie("scelta","visualizza", time()+3600);
 			 header("../Storage/Storage.php");
 			 }
 			 
   if($_POST['valore']==3){
- 			 setcookie("valore","COGNOME= , ID= ".$_COOKIE['id_utente'].", time()+3600);
+ 			 setcookie("valore","COGNOME= , ID= ".$_COOKIE['id_utente']." ", time()+3600);
   			 setcookie("scelta","visualizza", time()+3600);
 			 header("../Storage/Storage.php");
 			 }
