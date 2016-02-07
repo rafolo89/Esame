1509f0097c719617c77cefeb127429a31de81494
Ale1994 <alessandraorsi.94@libero.it>
2016-02-07 17:51:41 +0100

    GUIAlumno
    Aggiunto <script type="text/javascript" src="../js/inviavalore.js">
    </script> (richiamo a inviavalore)

--- Alumni/Presentation/GUIAlumno.php
+++ Alumni/Presentation/GUIAlumno.php
@@ -12,6 +12,7 @@
 <script type="text/javascript" src="../js/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="../js/script.js"></script>
 <script type="text/javascript" src="../js/coin-slider.min.js"></script>
+<script type="text/javascript" src="../js/inviavalore.js"> </script>
 <script LANGUAGE="JavaScript">
     <!-- 
     function openWindow()
