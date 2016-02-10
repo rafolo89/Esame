d97d05688be1663e17fe71c2bbf8b013c4338cbd
Ale1994 <alessandraorsi.94@libero.it>
2016-02-10 12:52:21 +0100

    Funzione javascript per la ricerca
    Ho aggiustato l'indentazione

--- Alumni/js/ricerca.js
+++ Alumni/js/ricerca.js
@@ -3,25 +3,25 @@ var xmlHttp
 
 function ricerca(campi){
 	
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			var valore=xmlhttp.responseText;
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
 			
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				var valore=xmlhttp.responseText;
+				
+				
+				
+			}
 			
-			
+		}	
+		string=nomefile+"?valore%5B%5D=";
+		i=0;
+		while(i< campi.length-1){
+			string=string + campi[i] +  "&valore%5B%5D=";
+			i++;
 		}
-		
-	}	
-	string=nomefile+"?valore%5B%5D=";
-	i=0;
-	while(i< campi.length-1){
-		string=string + campi[i] +  "&valore%5B%5D=";
-		i++;
-	}
-	string=string + campi[i] ;
-	xmlhttp.open("GET", string, false);
-    xmlhttp.send();
+		string=string + campi[i] ;
+		xmlhttp.open("GET", string, false);
+		xmlhttp.send();
 	
 }
